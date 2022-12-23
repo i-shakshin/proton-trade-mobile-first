c76a579d9e807e95aa63f7ef773db69dd1d24d0e
Ilya Shakshin <shakshin98@gmail.com>
2022-12-23 22:36:49 +0300

    added completed layout

--- js/script.js
+++ js/script.js
@@ -4,3 +4,31 @@ $(document).ready(function () {
     $("body").toggleClass("lock");
   });
 });
+
+const w = document.documentElement.clientWidth;
+
+function hideButton() {
+  const buttonUp = document.getElementsByClassName("button-up")[0];
+  const width = document.documentElement.clientWidth;
+  if (width >= 1200) {
+    buttonUp.style.display = "block";
+  } else {
+    buttonUp.style.display = "none";
+  }
+}
+
+window.addEventListener("load", function (event) {
+  hideButton();
+});
+
+window.addEventListener("resize", function (event) {
+  hideButton();
+});
+
+function topFunction() {
+  document.body.scrollTop = 0;
+}
+
+$("#form").submit(function (event) {
+  event.preventDefault();
+});
